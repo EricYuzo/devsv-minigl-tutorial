58ee5d73a9aa88d78ba12b17f0fba910917bc046
Eric Yuzo <eric_yuzo@yahoo.com.br>
2015-06-14 20:17:25 -0300

    Adicionando regra para state playing

--- meu-jogo/jogo.rb
+++ meu-jogo/jogo.rb
@@ -5,19 +5,24 @@ class MeuJogo < GameWindow
     def initialize
         super 800, 600, false
         @sprite = GameObject.new 0, 0, 72, 72, :img1, Vector.new(-14, -14)
+        @state = :new
     end
 
     def draw
         clear 0xabcdef
-        @sprite.draw
+        if @state == :playing
+            @sprite.draw
+        end
     end
 
     def update
-        KB.update
-        @sprite.x -= 3 if KB.key_down? Gosu::KbLeft
-        @sprite.x += 3 if KB.key_down? Gosu::KbRight
-        @sprite.y -= 3 if KB.key_down? Gosu::KbUp
-        @sprite.y += 3 if KB.key_down? Gosu::KbDown
+        if @state == :playing
+            KB.update
+            @sprite.x -= 3 if KB.key_down? Gosu::KbLeft
+            @sprite.x += 3 if KB.key_down? Gosu::KbRight
+            @sprite.y -= 3 if KB.key_down? Gosu::KbUp
+            @sprite.y += 3 if KB.key_down? Gosu::KbDown
+        end
     end
 
 end
