93372654bd75a0a8a7ae5d5ec56a156ea12d5e50
Eric Yuzo <eric_yuzo@yahoo.com.br>
2015-06-14 19:58:16 -0300

    Adicionando classe shuriken

---
+++ meu-jogo/shuriken.rb
@@ -0,0 +1,18 @@
+require 'minigl'
+include MiniGL
+
+class Shuriken < GameObject
+
+    def initialize(x, y, direction, speed)
+        super x, y, 20, 20, :shuriken, Vector.new(-5, -5), 2, 2
+        case direction
+            when :up    then @aim = Vector.new(@x, @y - 630)
+            when :right then @aim = Vector.new(@x + 830, @y)
+            when :down  then @aim = Vector.new(@x, @y + 630)
+            when :left  then @aim = Vector.new(@x - 830, @y)
+        end
+        @speed_m = speed
+    end
+
+end
+
